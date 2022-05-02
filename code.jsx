5de8bcf535e837d7366faf4f7243e8c7ca97a45a
Yivan2021 <manuel.rodriguez@softwareestrategico.com>
2022-05-02 09:11:22 -0500

    update

--- code.jsx
+++
@@ -1,7 +0,0 @@
-function Saludando(props) {
-    return <h1>Hello, {props.name}</h1>;
-  }
-  
-  const element = <Saludando name="Sara" />;
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(element);
